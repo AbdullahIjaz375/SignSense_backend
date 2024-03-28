c9b0613b41b9e0d79c9a90af66d2bc6e6212d4dd
Abdullahijaz375 <abdullahijaz375@gmail.com>
2024-03-29 03:19:08 +0500

    Updated image urls

--- utils/inverseMapping.js
+++ utils/inverseMapping.js
@@ -1,57 +1,57 @@
 const bucket = require("../utils/firebaseAdmin");
 
 const inverseMapping = {
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/A.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FA.png?alt=media`]:
     "A",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/B.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FB.png?alt=media`]:
     "B",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/C.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FC.png?alt=media`]:
     "C",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/D.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FD.png?alt=media`]:
     "D",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/E.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FE.png?alt=media`]:
     "E",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/F.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FF.png?alt=media`]:
     "F",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/G.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FG.png?alt=media`]:
     "G",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/H.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FH.png?alt=media`]:
     "H",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/I.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FI.png?alt=media`]:
     "I",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/J.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FJ.png?alt=media`]:
     "J",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/K.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FK.png?alt=media`]:
     "K",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/L.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FL.png?alt=media`]:
     "L",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/M.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FM.png?alt=media`]:
     "M",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/N.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FN.png?alt=media`]:
     "N",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/O.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FO.png?alt=media`]:
     "O",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/P.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FP.png?alt=media`]:
     "P",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/Q.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FQ.png?alt=media`]:
     "Q",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/R.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FR.png?alt=media`]:
     "R",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/S.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FS.png?alt=media`]:
     "S",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/T.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FT.png?alt=media`]:
     "T",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/U.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FU.png?alt=media`]:
     "U",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/V.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FV.png?alt=media`]:
     "V",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/W.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FW.png?alt=media`]:
     "W",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/X.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FX.png?alt=media`]:
     "X",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/Y.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FY.png?alt=media`]:
     "Y",
-  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL/Z.png?alt=media`]:
+  [`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/ASL%2FZ.png?alt=media`]:
     "Z",
 };
 
